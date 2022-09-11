6bc7e534a0772861a95b9f1d15ebb3578aa1f65b
tkyksasuka <110615564+tkyksasuka@users.noreply.github.com>
2022-09-11 14:11:13 +0000

    [question]14,15

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
   root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   #get 'post_images/new'
   #get 'post_images/index'
   #get 'post_images/show'
+
+  resources :users, only: [:show, :edit, :update]
+  #get 'users/show'
+  #get 'users/edit'
 
   get 'homes/about' => 'homes#about'
 
