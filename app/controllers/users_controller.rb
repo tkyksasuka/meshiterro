84f2ef056f0729f40327e45fa594c386a4262b9c
tkyksasuka <110615564+tkyksasuka@users.noreply.github.com>
2022-09-27 11:36:54 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id]) 
-    @post_images = @user.post_images
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(params[:id]) 
+    @user = User.find(params[:id])
   end
 
   def update
